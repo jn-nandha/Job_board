076c93037563a82fdb1d2aed51f0f30b255099a1
tejasviniparikh <tejal0003@gmail.com>
2018-01-31 14:20:38 +0530

    Registration and Edit profile Updated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :companies
+  
+  devise_for :companies, controllers: {
+        registrations: 'companies/registrations'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
-
 end
